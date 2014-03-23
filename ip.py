6df4601a763237e1eeae2ee69e2f4f669c2ae511
Jon Bloom <github@jon.tw>
2014-03-23 13:26:42 -0400

    ip

--- ip.py
+++ ip.py
@@ -1,24 +1,11 @@
-from flask import Flask, render_template
-from bs4 import BeautifulSoup
-from urllib2 import urlopen, Request
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-        url = 'http://www.gvsu.edu/'
-        req = Request(url)
-        req = urlopen(req)
-        soup = BeautifulSoup(req.read())
-        closed_div = soup.find("div", {"id": "gvsu-crisis_alert"})
-        if closed_div:
-            closed_div.h2.extract()
-            details = closed_div.text
-            closed = "Yes."
-        else:
-            closed = "No."
-            details = ""
-        return render_template('closed.html',closed=closed,details=details)
+    ip = request.remote_addr
+    return render_template('ip.html',ip=ip)
 
 if __name__ == "__main__":
     app.run()
